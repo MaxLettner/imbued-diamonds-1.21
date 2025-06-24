f5fc3168cfd0dbed1041b960b20c4a80c238b707
MaxLettner <lettnermaximilian2008@gmail.com>
2025-06-24 08:05:02 +0200

    Initial commit

---
+++ src/main/java/net/xeno2/imbued_diamonds/ImbuedDiamonds.java
@@ -0,0 +1,16 @@
+package net.xeno2.imbued_diamonds;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImbuedDiamonds implements ModInitializer {
+	public static final String MOD_ID = "imbued_diamonds";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/xeno2/imbued_diamonds/ImbuedDiamondsClient.java
@@ -0,0 +1,10 @@
+package net.xeno2.imbued_diamonds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImbuedDiamondsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
